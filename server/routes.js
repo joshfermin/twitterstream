68b3ed81d7f804e66c2864b7f019089b4023e43b
joshfermin <joshfermin@gmail.com>
2015-02-12 12:50:18 -0700

    adding streaming for tweets

--- server/routes.js
+++ server/routes.js
@@ -1,22 +1,27 @@
-var Twit 	 = require('twit'),
-	dotenv	 = require('dotenv');
+var Twit 	 = require('twit'), // wrapper on top of twitter api
+	dotenv	 = require('dotenv'); // used for keys -> get from .env
 
 module.exports = function(app) {
-dotenv.load();
+	dotenv.load();
 
-var T = new Twit({
-    consumer_key: 			process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: 		process.env.TWITTER_SECRET_KEY,
-    access_token: 			process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: 	process.env.TWITTER_ACCESS_KEY
-});
+	var T = new Twit({
+	    consumer_key: 			process.env.TWITTER_CONSUMER_KEY,
+	    consumer_secret: 		process.env.TWITTER_SECRET_KEY,
+	    access_token: 			process.env.TWITTER_ACCESS_TOKEN,
+	    access_token_secret: 	process.env.TWITTER_ACCESS_KEY
+	});
 
 
-app.get('/', function(req,res){
-	T.get('search/tweets', {q: 'banana since:2011-11-11', count: 100}, function(err, data, response){
-		res.send(data);
+	var stream = T.stream('statuses/sample')
+	stream.on('tweet', function(tweet){
+		console.log(tweet);
 	})
-});
+
+	app.get('/', function(req,res){
+		T.get('search/tweets', {q: 'banana since:2011-11-11', count: 100}, function(err, data, response){
+			res.send(data);
+		})
+	});
 
 
 }
